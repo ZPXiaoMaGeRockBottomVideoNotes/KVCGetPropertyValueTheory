f077e1dbb6cce22e47dcd83a228c4627bb7a09a4
Zhao Peng <nemozp@sina.cn>
2020-02-24 12:17:56 +0800

    修改批注。

--- KVC获取属性值原理/KVC获取属性值原理/ViewController.m
+++ KVC获取属性值原理/KVC获取属性值原理/ViewController.m
@@ -30,7 +30,6 @@
     /**
      通过KVC方式获取对象的属性值或者成员变量值的原理：
      当调用KVC的"valueForKey:"方法以后，系统会先在对象类的.m文件中按照getKey、key、isKey、_key的顺序查找这些实例方法，如果找到的话就返回这些方法里面的返回值，从而达到了通过KVC的方式获取对象的属性值的目的。如果上述的那些实例方法都没有找到的话就接着再在这个.m文件中寻找"accessInstanceVariablesDirectly"类方法，如果找到的话就查看这个方法的返回值，如果返回值为NO，则意味着不能访问这个对象类的成员变量，程序会崩溃，在控制台中会抛出异常"NSUnknownKeyException"。如果返回值为YES，则意味着可以直接访问这个对象类里面的成员变量，并且会按照_key、_isKey、key、isKey的顺序来查找成员变量，如果找到的话则获取它的值，如果还是没有找到的话则程序会崩溃，并且在控制台中会抛出异常"NSUnknownKeyException"。
-     
      */
     NSLog(@"%@", [person valueForKey:@"age"]);
 }
